745f6dab2ffd299e00f8c3c048abe671fbb280ac
swordsmanqaq <swords_man12@163.com>
2023-03-27 16:54:32 +0800

    banner.txt

--- src/main/java/com/heng/FoundationAppStart.java
+++ src/main/java/com/heng/FoundationAppStart.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
